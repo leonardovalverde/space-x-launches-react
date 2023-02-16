b3fd1de20d1285a8166c621075ce0f4da6cc9b9d
Leonardo Valverde <leovmsi@hotmail.com>
2023-02-16 04:17:56 -0300

    feat: reset style and remove strict

---
+++ src/index.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { createGlobalStyle } from "styled-components";
+import { colors } from "./ui-tokens/colors";
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    font-family: 'Roboto Condensed', sans-serif;
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    background-color: ${colors.secondary};
+  }
+`;
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(
+  <>
+    <GlobalStyle />
+    <App />
+  </>
+);
